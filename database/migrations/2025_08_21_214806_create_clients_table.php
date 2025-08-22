fcb9cda8c470cb1b025033f93d7923a7dd9da2c4
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2025-08-22 15:00:16 -0400

    models and migrations generate
    models basics generate.

---
+++ database/migrations/2025_08_21_214806_create_clients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('person_id');
+            $table->string('organization')->nullable();
+            $table->string('nit')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamps();
+
+            $table->foreign('person_id')->references('id')->on('people')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
