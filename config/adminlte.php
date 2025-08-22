4f0d0ad88198e163551cb76e08b933457512e9f4
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2025-08-22 15:00:58 -0400

    adminlte and livewire
    adminlte configured and livewire installed.

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>SOLUMIND</b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
@@ -154,8 +154,8 @@ return [
 
     'layout_topnav' => null,
     'layout_boxed' => null,
-    'layout_fixed_sidebar' => null,
-    'layout_fixed_navbar' => null,
+    'layout_fixed_sidebar' => true,
+    'layout_fixed_navbar' => true,
     'layout_fixed_footer' => null,
     'layout_dark_mode' => null,
 
@@ -300,11 +300,11 @@ return [
 
     'menu' => [
         // Navbar items:
-        [
-            'type' => 'navbar-search',
-            'text' => 'search',
-            'topnav_right' => true,
-        ],
+        // [
+        //     'type' => 'navbar-search',
+        //     'text' => 'search',
+        //     'topnav_right' => true,
+        // ],
         [
             'type' => 'fullscreen-widget',
             'topnav_right' => true,
@@ -312,84 +312,72 @@ return [
 
         // Sidebar items:
         [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
-        ],
-        [
-            'text' => 'blog',
-            'url' => 'admin/blog',
-            'can' => 'manage-blog',
-        ],
-        [
-            'text' => 'pages',
-            'url' => 'admin/pages',
-            'icon' => 'far fa-fw fa-file',
-            'label' => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
+            'text' => 'Tablero Principal',
             'url' => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'icon' => 'fas fa-fw fa-home',
+        ],
+        ['header' => 'Inventario'],
+        [
+            'text' => 'Productos',
+            'url' => 'admin/settings',
+            'icon' => 'fas fa-fw fa-box',
         ],
         [
-            'text' => 'change_password',
+            'text' => 'Proveedores',
             'url' => 'admin/settings',
             'icon' => 'fas fa-fw fa-lock',
         ],
         [
-            'text' => 'multilevel',
-            'icon' => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                        ],
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-            ],
+            'text' => 'Clientes',
+            'url' => 'admin/settings',
+            'icon' => 'fas fa-fw fa-users',
         ],
-        ['header' => 'labels'],
         [
-            'text' => 'important',
-            'icon_color' => 'red',
+            'text' => 'Entregas',
+            'url' => 'admin/settings',
+            'icon' => 'fas fa-fw fa-truck',
+        ],
+        ['header' => 'Operaciones'],
+        [
+            'text' => 'Historial de Movimientos',
+            'icon' => 'fas fa-fw fa-history',
             'url' => '#',
         ],
         [
-            'text' => 'warning',
-            'icon_color' => 'yellow',
+            'text' => 'Comprobantes',
+            'icon' => 'fas fa-fw fa-file-invoice',
             'url' => '#',
         ],
         [
-            'text' => 'information',
-            'icon_color' => 'cyan',
+            'text' => 'Libro Diario',
+            'icon' => 'fas fa-fw fa-book',
+            'url' => '#',
+        ],
+        [
+            'text' => 'Libro Mayor',
+            'icon' => 'fas fa-fw fa-book-open',
+            'url' => '#',
+        ],
+        ['header' => 'Reportes'],
+        [
+            'text' => 'Generar Reportes',
+            'icon' => 'fas fa-fw fa-history',
+            'url' => '#',
+        ],
+        ['header' => 'Opciones Avanzadas'],
+        [
+            'text' => 'Mi Perfil',
+            'icon' => 'fas fa-fw fa-user',
+            'url' => '#',
+        ],
+        [
+            'text' => 'Configuración',
+            'icon' => 'fas fa-fw fa-cog',
+            'url' => '#',
+        ],
+        [
+            'text' => 'Soporte',
+            'icon' => 'fas fa-fw fa-life-ring',
             'url' => '#',
         ],
     ],
@@ -546,5 +534,5 @@ return [
     |
     */
 
-    'livewire' => false,
+    'livewire' => true,
 ];
