4e30982e7624032b0662aac4445f52db7eb8daec
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2025-10-07 02:15:23 -0400

    fix position input products

--- resources/views/livewire/product-form.blade.php
+++ resources/views/livewire/product-form.blade.php
@@ -4,96 +4,189 @@
 
 <div>
     <div class="modal-body">
-        <div class="d-flex">
-            <div style="width: 50%;">
-                <label for="name">Nombre de Producto/Medida</label>
-                <div class="input-group">
-                    <input type="text" name="name" class="form-control mb-1" placeholder="Nombre del producto"
-                        wire:model.live="name" style="width: 40%;">
-                    <input type="text" placeholder="Medida" wire:model.live="size" class="form-control"
-                        style="width: 15%;">
-                </div>
-                <div class="text-danger" style="height: 20px;">
-                    @error('name')
-                        {{ $message }}
-                    @enderror
-                </div>
-                <label for="supplier">Proveedor</label>
-                <div class="input-group mb-1">
-                    <input type="number" name="supplier" class="form-control mb-1"
-                        placeholder="Ingrese NIT del proveedor" wire:model.live="nit">
-                    <span class="input-group-text" style="height: 38px;"><i class="nf {{ $code }}"></i></span>
-                    <a id="btn-searchable" class="btn btn-primary" style="height: 38px;"><i
-                            class="fa fa-search"></i></a>
-                </div>
-                <div id="searchable" class="d-none p-1 glow-border" wire:ignore.self>
-                    <label for="search">Buscar</label>
-                    <input type="text" name="search" class="form-control mb-3" wire:model.live="searchable"
-                        placeholder="Organización o Persona">
-                    <label for="search-item">Proveedor</label>
-                    <select name="search-item" class="form-select" style="height: 38px;"
-                        wire:model.live="item_searchable">
-                        <option>Seleccione Proveedor</option>
-                        @foreach ($list ?? [] as $item)
-                            <option value="{{ $item->nit }}">
-                                {{ ($item->organization ?? '') . ' (' . $item->person->name . ')' }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="text-danger" style="height: 20px;">
-                    @error('nit')
-                        {{ $message }}
-                    @enderror
-                </div>
-                <label for="price">Precio (Bs)</label>
-                <input type="number" name="price" class="form-control mb-1" placeholder="Ingrese precio del producto"
-                    wire:model="price">
-                <div class="text-danger" style="height: 20px;">
-                    @error('price')
-                        {{ $message }}
-                    @enderror
-                </div>
-                <label for="category">Categoria/Codigo</label>
-                <div class="input-group">
-                    <select class="form-select mb-1" name="category" wire:model.live="category" style="width: 40%;">
-                        <option>Seleccione una categoria</option>
-                        @foreach ($categories as $item)
-                            <option value="{{ $item->id }}">{{ $item->name . ' (' . $item->alias . ')' }}</option>
-                        @endforeach
-                    </select>
-                    <span class="input-group-text" style="height: 38px;">{{ $alias }}</span>
-                    <input type="text" class="form-control" wire:model="cod">
-                </div>
-                <div class="text-danger" style="height: 20px;">
-                    @error('category')
-                        {{ $message . '; ' }}
-                    @enderror
-                    @error('cod')
-                        {{ $message }}
-                    @enderror
-                </div>
-            </div>
-            <div style="width: 50%; margin-left: 10px;">
-                <label>Imagen del Producto</label>
-                <div class="d-flex">
-                    <div style="width: 100%;">
-                        <input type="file" name="image1" class="form-control mb-1" wire:model="img">
+        @if ($product->id == null)
+            <div class="d-flex">
+                <div style="width: 50%;">
+                    <label for="name">Nombre de Producto/Medida</label>
+                    <div class="input-group">
+                        <input type="text" name="name" class="form-control mb-1" placeholder="Nombre del producto"
+                            wire:model.live="name" style="width: 40%;">
+                        <input type="text" placeholder="Medida" wire:model.live="size" class="form-control"
+                            style="width: 15%;">
+                    </div>
+                    <div class="text-danger" style="height: 20px;">
+                        @error('name')
+                            {{ $message }}
+                        @enderror
+                    </div>
+                    <label for="supplier">Proveedor</label>
+                    <div class="input-group mb-1">
+                        <input type="number" name="supplier" class="form-control mb-1"
+                            placeholder="Ingrese NIT del proveedor" wire:model.live="nit">
+                        <span class="input-group-text" style="height: 38px;"><i
+                                class="nf {{ $code }}"></i></span>
+                        <a id="btn-searchable" class="btn btn-primary" style="height: 38px;"><i
+                                class="fa fa-search"></i></a>
+                    </div>
+                    <div id="searchable" class="d-none p-1 glow-border" wire:ignore.self>
+                        <label for="search">Buscar</label>
+                        <input type="text" name="search" class="form-control mb-3" wire:model.live="searchable"
+                            placeholder="Organización o Persona">
+                        <label for="search-item">Proveedor</label>
+                        <select name="search-item" class="form-select" style="height: 38px;"
+                            wire:model.live="item_searchable">
+                            <option>Seleccione Proveedor</option>
+                            @foreach ($list ?? [] as $item)
+                                <option value="{{ $item->nit }}">
+                                    {{ ($item->organization ?? '') . ' (' . $item->person->name . ')' }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="text-danger" style="height: 20px;">
+                        @error('nit')
+                            {{ $message }}
+                        @enderror
                     </div>
                 </div>
+                <div style="width: 50%; margin-left: 10px;">
+                    <label for="price">Precio (Bs)</label>
+                    <input type="number" name="price" class="form-control mb-1"
+                        placeholder="Ingrese precio del producto" wire:model="price">
+                    <div class="text-danger" style="height: 20px;">
+                        @error('price')
+                            {{ $message }}
+                        @enderror
+                    </div>
+                    <label for="category">Categoria/Codigo</label>
+                    <div class="input-group">
+                        <select class="form-select mb-1" name="category" wire:model.live="category" style="width: 40%;">
+                            <option>Seleccione una categoria</option>
+                            @foreach ($categories as $item)
+                                <option value="{{ $item->id }}">{{ $item->name . ' (' . $item->alias . ')' }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <span class="input-group-text" style="height: 38px;">{{ $alias }}</span>
+                        <input type="text" class="form-control" wire:model="cod">
+                    </div>
+                    <div class="text-danger" style="height: 20px;">
+                        @error('category')
+                            {{ $message . '; ' }}
+                        @enderror
+                        @error('cod')
+                            {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div>
+                <label for="description">Descripción</label>
+                <textarea class="form-control mb-1" name="description" rows="3" placeholder="Ingrese descripción del producto"
+                    wire:model="description"></textarea>
+            </div>
+            <h6><strong>Imagen del Producto</strong></h6>
+            <div class="d-flex">
+                <div style="width: 100%;">
+                    <input type="file" name="image1" class="form-control mb-1" wire:model="img">
+                </div>
+            </div>
+            @if (!empty($product->id))
+                <div class="d-flex justify-content-center">
+                    <img src="{{ $img }}" class="img-fluid" alt="Sin Imagen"
+                        style="width: auto;height: 300px;">
+                </div>
+            @endif
+        @else
+            <div class="d-flex">
+                <div style="width: 50%;">
+                    <label for="name">Nombre de Producto/Medida</label>
+                    <div class="input-group">
+                        <input type="text" name="name" class="form-control mb-1" placeholder="Nombre del producto"
+                            wire:model.live="name" style="width: 40%;">
+                        <input type="text" placeholder="Medida" wire:model.live="size" class="form-control"
+                            style="width: 15%;">
+                    </div>
+                    <div class="text-danger" style="height: 20px;">
+                        @error('name')
+                            {{ $message }}
+                        @enderror
+                    </div>
+                    <label for="supplier">Proveedor</label>
+                    <div class="input-group mb-1">
+                        <input type="number" name="supplier" class="form-control mb-1"
+                            placeholder="Ingrese NIT del proveedor" wire:model.live="nit">
+                        <span class="input-group-text" style="height: 38px;"><i
+                                class="nf {{ $code }}"></i></span>
+                        <a id="btn-searchable" class="btn btn-primary" style="height: 38px;"><i
+                                class="fa fa-search"></i></a>
+                    </div>
+                    <div id="searchable" class="d-none p-1 glow-border" wire:ignore.self>
+                        <label for="search">Buscar</label>
+                        <input type="text" name="search" class="form-control mb-3" wire:model.live="searchable"
+                            placeholder="Organización o Persona">
+                        <label for="search-item">Proveedor</label>
+                        <select name="search-item" class="form-select" style="height: 38px;"
+                            wire:model.live="item_searchable">
+                            <option>Seleccione Proveedor</option>
+                            @foreach ($list ?? [] as $item)
+                                <option value="{{ $item->nit }}">
+                                    {{ ($item->organization ?? '') . ' (' . $item->person->name . ')' }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="text-danger" style="height: 20px;">
+                        @error('nit')
+                            {{ $message }}
+                        @enderror
+                    </div>
+                    <label for="price">Precio (Bs)</label>
+                    <input type="number" name="price" class="form-control mb-1"
+                        placeholder="Ingrese precio del producto" wire:model="price">
+                    <div class="text-danger" style="height: 20px;">
+                        @error('price')
+                            {{ $message }}
+                        @enderror
+                    </div>
+                    <label for="category">Categoria/Codigo</label>
+                    <div class="input-group">
+                        <select class="form-select mb-1" name="category" wire:model.live="category"
+                            style="width: 40%;">
+                            <option>Seleccione una categoria</option>
+                            @foreach ($categories as $item)
+                                <option value="{{ $item->id }}">{{ $item->name . ' (' . $item->alias . ')' }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <span class="input-group-text" style="height: 38px;">{{ $alias }}</span>
+                        <input type="text" class="form-control" wire:model="cod">
+                    </div>
+                    <div class="text-danger" style="height: 20px;">
+                        @error('category')
+                            {{ $message . '; ' }}
+                        @enderror
+                        @error('cod')
+                            {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+                <div style="width: 50%; margin-left: 10px;">
+                    <label>Imagen del Producto</label>
+                    <div class="d-flex">
+                        <div style="width: 100%;">
+                            <input type="file" name="image1" class="form-control mb-1" wire:model="img">
+                        </div>
+                    </div>
 
-                @if (!empty($product->id))
-                    <div class="d-flex justify-content-center">
-                        <img src="{{ $img }}" class="img-fluid" alt="Sin Imagen"
-                            style="width: auto;height: 280px;">
-                    </div>
-                @endif
+                    @if (!empty($product->id))
+                        <div class="d-flex justify-content-center">
+                            <img src="{{ $img }}" class="img-fluid" alt="Sin Imagen"
+                                style="width: auto;height: 280px;">
+                        </div>
+                    @endif
+                </div>
             </div>
-        </div>
-        <div>
-            <label for="description">Descripción</label>
-            <textarea class="form-control mb-1" name="description" rows="3" placeholder="Ingrese descripción del producto"
-                wire:model="description"></textarea>
-        </div>
+        @endif
+
     </div>
     @can('product-permission', 3)
         @if (empty($product->id))
